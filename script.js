54ad31a3f29ed220addc6ce47044672e17278dab
Avanthika <avanthikasaravanan123@gmail.com>
2026-06-07 14:04:02 -0700

    Improved the styling and spacing, while testing affects on different parts of the UI.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+console.log("JS IS WORKING");
+
 document.querySelectorAll('.navbar a').forEach(link => {
     link.addEventListener('click', function(event) {
         event.preventDefault();
-       const target = document.querySelector(this.getAttribute('href'));
-       target.scrollIntoView({ behavior: "smooth"}); 
-    }); 
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
 });
 
 document.getElementById('shopBtn').addEventListener('click', function() {
@@ -14,4 +16,17 @@ const menuItems = document.querySelectorAll('.card');
 menuItems.forEach(item => {
     item.addEventListener('click', function() {
         alert(`You selected: ${this.querySelector('h3').textContent}`);
-    });});
+    });
+});
+
+const observer = new IntersectionObserver((entries) => { 
+    entries.forEach(entry => { 
+        if (entry.isIntersecting) { 
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".fade").forEach(el => {
+    observer.observe(el);
+});
